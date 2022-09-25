63580b0c0525def0b2952a945d9a2beaac8629fc
adamya-laptop <kumar.adamya2018@gmail.com>
2022-09-25 16:47:37 +0530

    test

--- src/Component/HeroSection/HeroSection.jsx
+++ src/Component/HeroSection/HeroSection.jsx
@@ -2,21 +2,67 @@ import React, { Fragment } from "react";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import "./HeroSection.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import "./styles.scss";
+
+// import required modules
+import { Pagination } from "swiper";
+
 const HeroSection = () => {
   return (
     <div className="landing">
-      
-      {/* <!-- INtro --> */}
       <div className="intro">
-        <h4>
-         Under
-        </h4>
-        <h4>Construction</h4>
-       
+        <>
+          <Swiper
+            breakpoints={{
+              // when window width is >= 640px
+              640: {
+                width: 640,
+                slidesPerView: 1,
+              },
+              // when window width is >= 768px
+              768: {
+                width: 768,
+                slidesPerView: 3,
+              },
+            }}
+            spaceBetween={30}
+            centeredSlides={true}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <div className="container">
+                <img src={require("../../Assets/card1.gif")} alt=""></img>
+              </div>{" "}
+              
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="container">
+                <img src={require("../../Assets/card3.gif")} alt=""></img>
+              </div>{" "}
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="container">
+                <img src={require("../../Assets/card3.gif")} alt=""></img>
+              </div>{" "}
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="container">
+                <img src={require("../../Assets/card1.gif")} alt=""></img>
+              </div>{" "}
+            </SwiperSlide>
+          </Swiper>
+        </>
       </div>
-     
-
-     
     </div>
   );
 };
